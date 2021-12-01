34d8369a349b504a69f8fae5ce8979b2963af18d
Elias Santander <cazagb@gmail.com>
2021-12-01 15:56:48 -0300

    Create engine.js

---
+++ js/engine.js
@@ -0,0 +1,18 @@
+console.log('>> engine.js loaded')
+modalTitle.innerHTML = `Fecha: ${dia}/${mes}`;
+
+function dPrint(){
+  print()
+}
+
+//populate printdoc
+//guardarTEMP
+/*
+var dataBaseOut = {'': 0};
+function saveDataBaseToLocalStorage() {
+  const dataBaseStr = JSON.stringify(dataBase);
+  localStorage.setItem("dataBaseStr", dataBaseStr);
+  var dataBaseOut = JSON.parse(localStorage.getItem("dataBaseStr"));
+  return dataBaseOut;
+}
+*/
