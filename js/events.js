e82bc71d8b75cb098da609c2523e525dda7214b2
Elias Santander <cazagb@gmail.com>
2021-12-01 15:57:35 -0300

    Add files via upload

---
+++ js/events.js
@@ -0,0 +1,13 @@
+console.log('>> events.js loaded')
+//shortcut keys
+document.addEventListener('keydown', keyShortcuts);
+//events btns 
+//reset dnote & temp DB
+btnReset.addEventListener("click", resetDnote);
+//save tempdb to actual db
+btnSave.addEventListener("click", saveDb);
+//event add to table
+btnPlus.addEventListener("click", createRemito);
+btnPreview.addEventListener("click", toDoc);
+console.log('v0.99')
+
