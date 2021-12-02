1541f677ecc6d2b57ad16e9777a2afbb81fb7ad0
Fernanda <fer@pop-os.localdomain>
2021-12-02 12:08:04 -0300

    updated to v2

---
+++ data/var.js
@@ -0,0 +1,277 @@
+console.log(">> var.js loaded");
+//inputfields, buttons n table
+const btnPlus = document.getElementById("btnPlus");
+const btnSave = document.getElementById("btnSave");
+const btnPreview = document.getElementById("btnPreview");
+const btnReset = document.getElementById("btnReset");
+const inputSele = document.getElementById("pansele");
+const inputCant = document.getElementById("cantidad");
+const dnote = document.getElementById("dnote");
+const dfooter = document.getElementById("dfooter");
+const totaldoc = document.getElementById("totaldoc");
+const modalTitle = document.getElementById("modalLabelone");
+const radiocheck = document.getElementById("flexRadioDefault1");
+const radiocheck2 = document.getElementById("flexRadioDefault2");
+const fecha = new Date();
+const dia = fecha.getUTCDate() + 1;
+const mes = fecha.getUTCMonth();
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLive = document.getElementById('liveToast')
+const toast = new bootstrap.Toast(toastLive)
+const modal2 = new bootstrap.Modal(document.getElementById('modal2'), {
+  keyboard: false
+})
+function addZero(i) {
+  if (i < 10) {i = "0" + i}
+  return i;
+}
+const d = new Date();
+let h = addZero(d.getHours());
+let m = addZero(d.getMinutes());
+let s = addZero(d.getSeconds());
+let time = h + ":" + m + ":" + s;
+
+//total pending
+var total = 0;
+//maindb
+const db = [
+  {
+    id: "bagel",
+    name: "Bagel",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "briocheMolde",
+    name: "Brioche Molde 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "clasicoMolde1kg",
+    name: "Clasico Molde 1kg",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "clasico500",
+    name: "Clasico 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "pizza",
+    name: "Pizza Pack x2",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "ciabatta",
+    name: "Ciabatta",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "maiz500",
+    name: "Maiz 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "mediterraneo500",
+    name: "Mediterraneo 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "muesli500",
+    name: "Muesli 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "multibun",
+    name: "Multicereal Bun",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "multimolde",
+    name: "Multicereal Molde 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "bunpapa12u",
+    name: "Bun Papa 12u",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "bunpapamarket",
+    name: "Bun Papa Market",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "rusticoMolde1kg",
+    name: "Rustico Molde 1kg",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "rustico500",
+    name: "Rustico 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "semillasMolde1kg",
+    name: "Semillas Molde 1kg",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "semillas500",
+    name: "Semillas 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "trigo500",
+    name: "Trigo 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "rugbrod800",
+    name: "Rugbrod 800g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+];
+
+//tempdb
+
+const dbTemp = [
+  {
+    id: "bagel",
+    name: "Bagel",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "briocheMolde",
+    name: "Brioche Molde 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "clasicoMolde1kg",
+    name: "Clasico Molde 1kg",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "clasico500",
+    name: "Clasico 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "pizza",
+    name: "Pizza Pack x2",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "ciabatta",
+    name: "Ciabatta",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "maiz500",
+    name: "Maiz 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "mediterraneo500",
+    name: "Mediterraneo 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "muesli500",
+    name: "Muesli 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "multibun",
+    name: "Multicereal Bun",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "multimolde",
+    name: "Multicereal Molde 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "bunpapa12u",
+    name: "Bun Papa 12u",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "bunpapamarket",
+    name: "Bun Papa Market",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "rusticoMolde1kg",
+    name: "Rustico Molde 1kg",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "rustico500",
+    name: "Rustico 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "semillasMolde1kg",
+    name: "Semillas Molde 1kg",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "semillas500",
+    name: "Semillas 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "trigo500",
+    name: "Trigo 500g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+  {
+    id: "rugbrod800",
+    name: "Rugbrod 800g",
+    cantidadb2b: 0,
+    cantidadb2c: 0,
+  },
+];
+
+const udb = {
+  biomarket: {name: 'Biomarket', et: 'no', prefs: {mediterraneo500: 1, rustico500: 1, semillasMolde1kg: 3}},
+  kelpMarket: {name: 'Kelp Market', et: 'si', prefs: {clasico500: 1, maiz: 1, rustico500: 1, semillas500: 2}},
+  harper: {name: 'Harper', et: 'no', prefs: {clasicoMolde1kg: 1, rustico500: 1, semillasMolde1kg: 3}},
+}
