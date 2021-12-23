b70ce3e17ba5816705c5f75cd9cbff57fd90d50c
Elias Santander <cazagb@gmail.com>
2021-12-23 19:14:53 -0300

    Update events.js

--- js/events.js
+++ js/events.js
@@ -9,6 +9,10 @@ btnSave.addEventListener("click", saveDb);
 //event add to table
 btnPlus.addEventListener("click", createRemito);
 btnPreview.addEventListener("click", toDoc);
-btnPlus.addEventListener("focus", highlight)
-btnPreview.addEventListener("focus", highlight)
-btnSave.addEventListener("focus", highlight)
+btnPlus.addEventListener("focus", highlight(btnPlus))
+btnPreview.addEventListener("focus", highlight(btnPreview))
+btnSave.addEventListener("focus", highlight(btnSave))
+inputSele.addEventListener("focus", highlight(inputSele))
+cantSele.addEventListener("focus", highlight(cantSele))
+btnReset.addEventListener("focus", highlight(btnReset));
+
