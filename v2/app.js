881adfa70200c67bec68461fdd2a29a46ddf258c
Elias Santander <cazagb@gmail.com>
2022-06-05 16:38:47 -0300

    small tweaks

--- v2/app.js
+++ v2/app.js
@@ -1,8 +1,21 @@
 console.log('console')
 // regexp
 let text = "some random text to start ";
+let source = "text to search from";
 let pattern = /text/i;
 let result = text.match(pattern);
+
+const dataBase = [{key: 'some random text', key: 'heres some more text to search in', key: 'a little more text'}]
+function saveDb() {
+  for (var i = 0; i < databMap.lenght; i++) {
+  let bagel = /bagel/i;
+  let target = dataBase[i].key
+    db[i].cantidadb2b += dbTemp[i].cantidadb2b
+    dbTemp[i].cantidadb2b = 0
+    db[i].cantidadb2c += dbTemp[i].cantidadb2c
+    dbTemp[i].cantidadb2c = 0
+  }
+
 console.log(result)
 document.getElementById('writableDiv').innerHTML = `encontre: ${result}`;
 //shortcut keys
